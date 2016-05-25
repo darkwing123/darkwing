9d51513256bd89ab4b1e48fc3cf00df710ebd2aa
李志彬 <lizhibin@lizhibindeMacBook-Air.local>
2016-05-25 15:57:59 +0800

    改动了一些东西

--- UIJgithub/UIJgithub/ViewController.m
+++ UIJgithub/UIJgithub/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    int number = 0;
+    int sum = number++;
+    NSLog(@"sum____%d",sum);
 }
 
 - (void)didReceiveMemoryWarning {
