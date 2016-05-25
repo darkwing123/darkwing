5530f1b56ac16298f2d710a471bbfb09220b90d5
李志彬 <lizhibin@lizhibindeMacBook-Air.local>
2016-05-25 15:50:27 +0800

    我新建了些项目

---
+++ UIJgithub/UIJgithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIJgithub
+//
+//  Created by 李志彬 on 16/5/25.
+//  Copyright © 2016年 李志彬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIJgithub/UIJgithub/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIJgithub
+//
+//  Created by 李志彬 on 16/5/25.
+//  Copyright © 2016年 李志彬. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
